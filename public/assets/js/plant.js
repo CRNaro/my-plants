2d879e1549e90833725bff62df1e3eded64c83bf
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-01-28 14:30:13 -0500

    Build/Fix: Have img rendering

--- public/assets/js/plant.js
+++ public/assets/js/plant.js
@@ -53,38 +53,35 @@ function renderPlants(plants) {
 //   const plantInfoContainer = document.getElementById("plant-info");
   searchResultsContainer.innerHTML = "";
   return plants.map(plant => {
-    console.log(plant.common_name)
+    console.log("Hello Plants", plant.common_name)
     const plantCard = document.createElement("div");
     plantCard.className = "plant-card";
 // //console.log("plants!?!?!", renderPlants)
     const plantName = document.createElement("h3");
     plantName.textContent = plant.common_name;
    
-//     const plantImage = document.createElement("img");
-//     plantImage.src = plant.default_image?.regular_url || "default_image_url"
-//     //plant.default_image.regular_url || "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",  // need to set the image
-//     plantImage.alt = `Image of ${plant.common_name}`;
-
-//     const addButton = document.createElement("button");
+     const plantImage = document.createElement("img");
+     plantImage.src = plant.default_image.regular_url || "default_image_url"
+    //plant.default_image.regular_url || "https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/regular/49255769768_df55596553_b.jpg",  // need to set the image
+   plantImage.alt = `Image of ${plant.common_name}`;
+  console.log("IMAGE!!!!", plantImage)
+    const addButton = document.createElement("button");
 //     addButton.textContent = "Add to My List";
 //     addButton.dataset.plantId = plant.id; // Assign the plant's id to the button's data attribute
     
     //addButton.addEventListener("click", savePlantToAccount);
-
-   plantCard.append(plantName);  // plantImage, addButton
+ 
+   plantCard.append(plantName, plantImage, addButton);  // plantImage, addButton
 
    searchResultsContainer.append(plantCard);
 
 //     const plantInfo = document.createElement("div");
 //     plantInfo.className = "search-results";  //not plant-info
-//     plantInfo.innerHTML = `
-//     <h2 id="plant-name">${plant.common_name}</h2>
-//     <p id="description">${plant.description}</p>
-//     <p id="watering">${plant.watering}</p>
-//     <p id="sun">${plant.sunlight}</p>
-//     <p id="poison">${plant.poisonous_to_pets}</p>
-//     <img id="image" src=${plant.default_image.regular_url} alt="Image of ${plant.common_name}">
-//     `;
+   plantInfo.innerHTML = `
+     <h2 id="plant-name">${plant.common_name}</h2>
+
+    <img id="image" src=${plant.default_image.regular_url} alt="Image of ${plant.common_name}">
+  `;
 //     plantInfoContainer.appendChild(plantInfo);
 //     console.log("Plants please!!!", plantInfo);
   });
@@ -118,3 +115,7 @@ submitBtn.addEventListener('click', searchPlants);
 const addBtn = document.getElementById("add-btn");
 addBtn.addEventListener('click', addPlant);
 
+//     <p id="description">${plant.description}</p>
+//     <p id="watering">${plant.watering}</p>
+//     <p id="sun">${plant.sunlight}</p>
+//     <p id="poison">${plant.poisonous_to_pets}</p>
